deaa9694fc907378ef7116d79890a25b0a7bb074
Matthew Bender <benderm@gmail.com>
2014-09-22 07:52:22 -0600

    random forest

---
+++ random_forest.R
@@ -0,0 +1,27 @@
+# load dependencies
+library(randomForest)
+
+# load train and test data
+train <- read.csv("Data/train.csv", stringsAsFactors=FALSE)
+test <- read.csv("Data/test.csv", stringsAsFactors=FALSE)
+
+# create useable data from datetime
+train$hour <- factor(format(as.POSIXct(train$datetime, format="%Y-%m-%d %H:%M"), format="%H"))
+train$month <- factor(format(as.POSIXct(train$datetime, format="%Y-%m-%d %H:%M"), format="%B"))
+train$weekday <- factor(format(as.POSIXct(train$datetime, format="%Y-%m-%d %H:%M"), format="%u"))
+train$year <- factor(format(as.POSIXct(train$datetime, format="%Y-%m-%d %H:%M"), format="%y"))
+test$hour <- factor(format(as.POSIXct(test$datetime, format="%Y-%m-%d %H:%M"), format="%H"))
+test$month <- factor(format(as.POSIXct(test$datetime, format="%Y-%m-%d %H:%M"), format="%B"))
+test$weekday <- factor(format(as.POSIXct(test$datetime, format="%Y-%m-%d %H:%M"), format="%u"))
+test$year <- factor(format(as.POSIXct(test$datetime, format="%Y-%m-%d %H:%M"), format="%y"))
+
+set.seed(415)
+fit <- randomForest(as.factor(count) ~ hour + month + year + weekday + weather + atemp +
+                      workingday + holiday + windspeed + humidity + season, data=train, ntree = 700, importance=TRUE)
+
+prediction <- predict(fit, test)
+
+# Create submission dataframe and output to file
+submit <- data.frame(datetime = test$datetime, count = as.numeric(prediction))
+write.csv(submit, file = "Output/random_forest.csv", row.names = FALSE)
+
